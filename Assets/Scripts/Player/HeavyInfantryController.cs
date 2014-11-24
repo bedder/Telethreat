051e3572949fb6d7839088c2f471f517f8604f3e
bedder <bedder@gmail.com>
2014-11-24 15:15:26 +0000

    Added grenades and Heavy Infantry action

--- Assets/Scripts/Player/HeavyInfantryController.cs
+++ Assets/Scripts/Player/HeavyInfantryController.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class HeavyInfantryController : PlayerController {
-    public override void performClassAction() { // TODO
-        Debug.LogError("HeavyInfantryController::performClassAction is not yet implemented.");
+    public Rigidbody grenade;
+    public Transform leftHand;
+
+    public override void performClassAction() { // Magic numbers chosen by magic.
+        Rigidbody clone;
+        Vector3 throwDirectionEuler = Vector3.Lerp(transform.forward, Vector3.up, 0.4f);
+        clone = (Rigidbody)Instantiate(grenade, leftHand.position, Quaternion.LookRotation(throwDirectionEuler));
+        clone.AddForce(throwDirectionEuler * 600);
     }
 }
