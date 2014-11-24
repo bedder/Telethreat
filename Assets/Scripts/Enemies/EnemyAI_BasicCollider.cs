ff2f15f11b24d8d5daf5150241255c41e8415c4c
Nick Sephton <njs523@york.ac.uk>
2014-11-24 16:13:33 +0000

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/Scripts/Player.meta

---
+++ Assets/Scripts/Enemies/EnemyAI_BasicCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI_BasicCollider : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    private Transform Target;
+    private CharacterController controller;
+    private NavMeshAgent navAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+        Target = GameObject.FindWithTag("Player").transform;
+        controller = GetComponent<CharacterController>();
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //If we can lock the player
+        if (Target != null)
+        {
+            navAgent.updatePosition = true;
+            navAgent.updateRotation = true;
+            navAgent.SetDestination(Target.position);   
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/MeleeAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour
+{
+    public float AttackRate;
+    public float AttackDamage;
+    //private CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        //controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.LogWarning("TEST (CHARACTERCONTROLLER)");
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.LogWarning("Test");
+    }
+}
