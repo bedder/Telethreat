5221f6406866b8d2e73f7c89d836c9cfa551d1b4
bedder <bedder@gmail.com>
2014-11-25 11:30:30 +0000

    Refactored GUI code

--- Assets/Scripts/GUISliders.cs
+++ Assets/Scripts/GUISliders.cs
@@ -4,36 +4,74 @@ using System.Collections;
 public class GUISliders : MonoBehaviour {
     private PlayerController player;
     private Gun gun;
-    private int barHeight = 20;
-    private int barWidth;
-    private int barPad = 6;
+    private int healthbarHeight = 20;
+    private int healthbarWidth; // Set automatically w/ respect to screen width
+    private int energybarHeight = 200;
+    private int energybarWidth = 10;
+    private int pad = 6;
+
+    private Rect healthGroup;
+    private Rect healthbarActual;
+    private Rect healthbarFull;
+    private Rect armourMeter;
+    private Rect armourbarActual;
+    private Rect armourbarFull;
+
+    private Rect energyGroup;
+    private Rect energybarActual;
+    private Rect energybarFull;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         gun = player.gun;
-        barWidth = (int)(Screen.width - (3 * barPad)) / 2;
+
+        healthbarWidth = (int)(Screen.width - (3 * pad)) / 2;
+        
+        healthGroup = new Rect(pad, pad, 2 * (pad + healthbarWidth), 2 * healthbarHeight);
+        armourbarActual = new Rect(0, 0, healthbarWidth, healthbarHeight);
+        armourbarFull   = new Rect(0, 0, healthbarWidth, healthbarHeight);
+        healthbarActual = new Rect(pad + healthbarWidth, 0, healthbarWidth, healthbarHeight);
+        healthbarFull   = new Rect(pad + healthbarWidth, 0, healthbarWidth, healthbarHeight);
+        
+        energyGroup = new Rect(Screen.width - pad - energybarWidth, Screen.height - pad - energybarHeight,
+                               energybarWidth, energybarHeight);
+        energybarActual = new Rect(0, 0, energybarWidth, energybarHeight);
+        energybarFull   = new Rect(0, 0, energybarWidth, energybarHeight);
     }
 
     void OnGUI() {
-        float health = player.health / 100f;
-        float armour = player.armour / 100f;
+        updateValues();
+
         GUIStyle leftStyle = new GUIStyle();
         GUIStyle rightStyle = new GUIStyle();
         leftStyle.alignment = TextAnchor.UpperLeft;
         rightStyle.alignment = TextAnchor.UpperRight;
 
-        GUI.BeginGroup(new Rect(barPad, barPad, 2 * (barPad + barWidth), 2 * barHeight)); // Health and Armour
-            GUI.Box(new Rect(0, 0, barWidth, barHeight), "");
-            GUI.Box(new Rect(barWidth + barPad, 0, barWidth, barHeight), "");
-            GUI.Box(new Rect((barWidth * (1 - armour)), 0, barWidth * armour, barHeight), "");
-            GUI.Box(new Rect(barWidth + barPad, 0, barWidth * health, barHeight), "");
-            GUI.Label(new Rect(0, barHeight, barWidth - barPad, barHeight), "ARMOUR", rightStyle);
-            GUI.Label(new Rect(barWidth + 2 * barPad, barHeight, barWidth - barPad, barHeight), "HEALTH", leftStyle);
+        GUI.BeginGroup(healthGroup); // Health and Armour
+            GUI.Box(armourbarFull, "");
+            GUI.Box(armourbarActual, "");
+            GUI.Box(healthbarFull, "");
+            GUI.Box(healthbarActual, "");
+            GUI.Label(new Rect(0, healthbarHeight, healthbarWidth - pad, healthbarHeight), "ARMOUR", rightStyle);
+            GUI.Label(new Rect(healthbarWidth + 2 * pad, healthbarHeight, healthbarWidth - pad, healthbarHeight), "HEALTH", leftStyle);
         GUI.EndGroup();
 
-        GUI.BeginGroup(new Rect(Screen.width - barPad - barHeight, Screen.height - barPad - 100, barHeight, 100)); // Energy
-            GUI.Box(new Rect(0, 0, barHeight, 100), "");
-            GUI.Box(new Rect(0, (100 - gun.energy), barHeight, gun.energy), "");
+        GUI.BeginGroup(energyGroup);
+            GUI.Box(energybarFull, "");
+            GUI.Box(energybarActual, "");
         GUI.EndGroup();
+    }
+
+    void updateValues() {
+        if (player != null) {
+            armourbarActual.x = healthbarWidth * (1 - player.armour / 100f);
+            armourbarActual.width = healthbarWidth * (player.armour / 100f);
+            healthbarActual.width = healthbarWidth * (player.health / 100f);
+        }
+
+        if (gun != null) {
+            energybarActual.y = energybarHeight * (1 - gun.energy / 100f);
+            energybarActual.height = energybarHeight * (gun.energy / 100f);
+        }
     }
 }
