6b3ea081500d1c895ef0069cc0d99a29fd287595
Nick Sephton <njs523@york.ac.uk>
2014-11-25 13:06:53 +0000

    MeleeAttack working, Collider closes to player and attacks for 40.

--- Assets/Scripts/Enemies/MeleeAttack.cs
+++ Assets/Scripts/Enemies/MeleeAttack.cs
@@ -3,28 +3,40 @@ using System.Collections;
 
 public class MeleeAttack : MonoBehaviour
 {
-    public float AttackRate;
+    public float AttackRechargeTime;
     public float AttackDamage;
+    public float AttackRange;
+
+    private GameObject player;
+    private float lastAttack;
 
     // Use this for initialization
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
+        lastAttack = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Debug.LogWarning("TEST (OnControllerColliderHit) - " + hit.collider.gameObject.name);
-    }
-
-
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.LogWarning("Test (OnCollisionEnter) - " + collision.collider.gameObject.name);
+        if ((lastAttack + AttackRechargeTime) <= Time.time)
+        {
+            if (Vector3.Distance(player.transform.position, this.transform.position) <= AttackRange)
+            {
+                //Do Attack
+                PlayerController damPlayer = player.GetComponent<PlayerController>();
+                damPlayer.damage(AttackDamage);
+                lastAttack = Time.time;
+                Debug.LogWarning(string.Format("Attack for {0} ({1}/{2}, {3}/{4})", AttackDamage, damPlayer.health, damPlayer.maxHealth, damPlayer.armour, damPlayer.maxArmour));
+            }
+            //else
+            //{
+            //    Debug.LogWarning("No target in range");
+            //}
+        }
+        //else
+        //{
+        //    Debug.LogWarning("Awaiting attack recharge");
+        //}
     }
 }
