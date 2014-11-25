4ab9c22f54babc62111f824afebb75d0e8f430b0
Christian Guckelsberger <c.guckelsberger@gold.ac.uk>
2014-11-25 16:17:40 +0100

    Improved game graph performance, computed teleporting areas

--- Assets/Scripts/StaticLevelGenerator.cs
+++ Assets/Scripts/StaticLevelGenerator.cs
@@ -60,19 +60,22 @@ public class StaticLevelGenerator : MonoBehaviour
 		}
 		graphCells.addNode (new Node (m_points [m_points.Count - 1], Node.CellType.goal, id++));
 
+
 		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		sphere.transform.position = new Vector3(m_points [0].x-m_mapWidth/2,0.5f,m_points [0].y-m_mapHeight/2);
-
+		/*
 		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		sphere.transform.position = new Vector3(m_points [m_points.Count - 1].x-m_mapWidth/2,0.5f,m_points [m_points.Count - 1].y-m_mapHeight/2);
-		
+		*/
+
 		//Set adjacent cells in graphCells
 		foreach(Node n in graphCells.Nodes ()) {
 			List<Vector2> neighbors = v.NeighborSitesForSite (n.coords);
 			foreach(Vector2 neighborCoords in neighbors){
 				Node nNeighbor = graphCells.getNode(neighborCoords);
-				n.addAdjacent(nNeighbor);
-				nNeighbor.addAdjacent(n);
+				List<Vector2> edge = commonLine(n,nNeighbor,v);
+				n.addAdjacent(nNeighbor, edge[0], edge[1]);
+				nNeighbor.addAdjacent(n, edge[0], edge[1]);
 			}
 		}
 		
@@ -92,8 +95,10 @@ public class StaticLevelGenerator : MonoBehaviour
 			//Cells that are connected by a teleporter will be represented as adjacent in graphTele
 			Node n1 = graphTele.getNode(coordsSite1);
 			Node n2 = graphTele.getNode(coordsSite2);
-			n1.addAdjacent(n2);
-			n2.addAdjacent(n1);
+
+			List<Vector2> commonEdge = graphCells.getNode(n1.id).getEdgeToAdjacent(n2);
+			n1.addAdjacent(n2,commonEdge[0],commonEdge[1]);
+			n2.addAdjacent(n1,commonEdge[0],commonEdge[1]);
 			
 			/*
 			foreach (LineSegment line in m_edges) {
@@ -126,14 +131,16 @@ public class StaticLevelGenerator : MonoBehaviour
 		remaining.Add (8);
 		remaining.Add (5);
 		remaining.Add (6);
-		remaining.Add (9);
 		remaining.Add (8);
+		remaining.Add (9);
 		for(int i=1; i<remaining.Count; i++){
 			//Cells that are connected by a teleporter will be represented as adjacent in graphTele
 			Node n1 = graphTele.getNode(remaining[i-1]);
 			Node n2 = graphTele.getNode(remaining[i]);
-			n1.addAdjacent(n2);
-			n2.addAdjacent(n1);
+
+			List<Vector2> commonEdge = graphCells.getNode(n1.id).getEdgeToAdjacent(n2);
+			n1.addAdjacent(n2,commonEdge[0],commonEdge[1]);
+			n2.addAdjacent(n1,commonEdge[0],commonEdge[1]);
 
 			//Instantiate teleporter at wall
 			List<Vector2> commonPoints = commonLine(n1, n2, v);
@@ -171,6 +178,11 @@ public class StaticLevelGenerator : MonoBehaviour
 		floor.transform.localScale = new Vector3 (10.0f,1.0f,5.0f);
 		floor.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
 		floor.transform.parent = transform;
+
+		//getTeleportAreas (graphCells.getNode(m_points[0]), v, graphCells, graphTele);
+
+
+		transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
 		
 	}
 
@@ -220,4 +232,102 @@ public class StaticLevelGenerator : MonoBehaviour
 		}
 	}
 
+	private Dictionary<List<Vector2>,Node> getTeleportAreas(Node n, Delaunay.Voronoi v, GameGraph graphCells, GameGraph graphTele){
+		Vector2 middle = n.coords;
+		List<List<Vector2>> ordinaryTris = new List<List<Vector2>> ();
+		Dictionary<List<Vector2>,Node> subdivision = new Dictionary<List<Vector2>,Node> ();
+
+		List<Node> adjacentCells = graphCells.getNode (middle).getAdjacent ();
+		foreach(Node adj in adjacentCells){
+			List<Vector2> edge = graphTele.getNode(middle).getEdgeToAdjacent(adj);
+			if(edge !=null){
+				edge.Add(middle);
+				subdivision.Add(edge, adj);
+			}else{
+				edge = graphCells.getNode(middle).getEdgeToAdjacent(adj);
+				edge.Add (middle);
+				ordinaryTris.Add (edge);
+			}
+		}
+
+		adjacentCells = graphTele.getNode (middle).getAdjacent ();
+		foreach(List<Vector2> tri in ordinaryTris){
+			float minDistance = float.MaxValue;
+			Node targetCell = null;
+			foreach (Node adj in adjacentCells) {
+				List<Vector2> edge = graphTele.getNode(middle).getEdgeToAdjacent(adj);
+				Vector2 edgeMiddle = (edge[0] + edge[1])/2.0f;
+				Vector2 triMiddle = (tri[0]+tri[1]+tri[2])/3.0f;
+				float distance = Vector2.Distance(edgeMiddle,triMiddle);
+				if(distance<minDistance){
+					minDistance=distance;
+					targetCell = adj;
+				}
+			}
+			subdivision.Add(tri, targetCell);
+		}
+		List<Vector2> t = subdivision.Keys.ToList ()[0];
+
+		/*
+		Mesh msh = new Mesh();
+		msh.vertices = new Vector3[]{new Vector3(t[0].x,0.0f,t[0].y),new Vector3(t[1].x,0.0f,t[1].y),new Vector3(t[2].x,0.0f,t[2].y)};
+		msh.triangles = new int[]{0,1,2};
+		msh.RecalculateNormals();
+		msh.RecalculateBounds();
+		
+		// Set up game object with mesh;
+		gameObject.AddComponent(typeof(MeshRenderer));
+		//MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+		//filter.mesh = msh;
+		gameObject.AddComponent<MeshFilter>().mesh=msh;
+*/
+		Mesh mesh = new Mesh ();
+		if(!transform.GetComponent<MeshFilter> () || !transform.GetComponent<MeshRenderer> () ) //If you will havent got any meshrenderer or filter
+		{
+			transform.gameObject.AddComponent<MeshFilter>();
+			transform.gameObject.AddComponent<MeshRenderer>();
+		}
+
+		Vector3 p0 = new Vector3(0,0,0);
+		Vector3 p1 = new Vector3(1,0,0);
+		Vector3 p2 = new Vector3(0.5f,0,Mathf.Sqrt(0.75f));
+		Vector3 p3 = new Vector3(0.5f,Mathf.Sqrt(0.75f),Mathf.Sqrt(0.75f)/3);
+
+		mesh.Clear();
+		mesh.vertices = new Vector3[]{new Vector3(t[0].x,0.0f,t[0].y),new Vector3(t[1].x,0.0f,t[1].y),new Vector3(t[2].x,0.0f,t[2].y)};
+		mesh.triangles = new int[]{
+			2,1,0
+		};
+		
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+		mesh.Optimize();
+		
+		GameObject triangle = new GameObject ();
+		triangle.name = "Triangle";
+		triangle.AddComponent (typeof(MeshFilter));
+		triangle.AddComponent (typeof(MeshRenderer));
+		triangle.GetComponent<MeshFilter> ().mesh = mesh;
+
+		Material myNewMaterial = new Material(Shader.Find("Transparent/Diffuse"));
+		myNewMaterial.SetColor ("_Color", new Color (0.0f, 0.0f, 1.0f, 0.7f)); //Semi-transparent blue
+		triangle.renderer.material = myNewMaterial;
+		//triangle.transform.localRotation = Quaternion.Euler(180,0.0f,0.0f);
+
+		Vector2 triMid=	(t [0] + t [1] + t [2]) / 3.0f;
+
+		gameObject.transform.localPosition=new Vector3(triMid.x, 0.0f, triMid.y);
+
+		triangle.transform.parent = transform;
+
+		
+		/*
+		Vector2 triMid = (t[0]+t[1]+t[2])/(1/3.0f);
+		gameObject.transform.position=new Vector3(triMid.x, 0.0f, triMid.y);
+		gameObject.transform.parent = transform;*/
+		
+		return subdivision;
+	}
+	
+	
 }
