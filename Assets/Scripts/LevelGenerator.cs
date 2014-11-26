ac3308b6b9b1c8f2b66fd5b1005556325a9bdad5
Christian Guckelsberger <c.guckelsberger@gold.ac.uk>
2014-11-26 23:44:47 +0100

    Added procedurally generated floor. Fixed teleport influence areas.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Delaunay;
 using Delaunay.Geo; 
 
@@ -27,11 +28,16 @@ public class LevelGenerator : MonoBehaviour
 	private List<Vector2> m_points;
 	private List<LineSegment> m_edges;
 
+	private GameObject gameObject_triangle;
+	private GameObject gameObject_walls;
+	private GameObject gameObject_floor;
+	private GameObject gameObject_teleporters;
+	
 	List<Rectangle> rects;
 
 	public GameObject prefab_teleporter;
 	public GameObject prefab_wall;
-	public GameObject prefab_floor;
+	public Material material_floor;
 	public int m_pointCount = 100;
 	public float m_mapWidth = 100;
 	public float m_mapHeight = 50;
@@ -51,45 +57,48 @@ public class LevelGenerator : MonoBehaviour
 		Vector2[] startGoalCells;
 		GameGraph graphCells;
 		List<Node> path = null;
-		m_enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+		m_enemies = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Enemy"));
 		for (int i=0; i<moreColors.Count(); i++) {
-			colors.Add (moreColors[i]);
+				colors.Add (moreColors [i]);
 		}
-
+		gameObject_triangle = new GameObject ("Triangles");
+		gameObject_triangle.transform.parent = transform;
+		gameObject_floor = new GameObject ("Floor");
+		gameObject_floor.transform.parent = transform;
+		gameObject_teleporters = new GameObject ("Teleporters");
+		gameObject_teleporters.transform.parent = transform;
+		gameObject_walls = new GameObject ("Walls");
+		gameObject_walls.transform.parent = transform;
+		
 		//Create level and check if path from start- to goal cell exists (left to right). Otherwise, repeat.
 		do {
-			startGoalCells = GenerateLevel ();
-			Vector2 startCell = startGoalCells[0];
-			Vector2 goalCell = startGoalCells[1];
-		
-			//Create graphCells with nodes representing cells
-			graphCells = createGameGraph (v,startCell,goalCell);
+				startGoalCells = GenerateLevel ();
+				Vector2 startCell = startGoalCells [0];
+				Vector2 goalCell = startGoalCells [1];
 
-			path = graphCells.BFS (graphCells.getNode (startCell), graphCells.getNode (goalCell));
+				//Create game graph with nodes representing cells
+				graphCells = createGameGraph (v, startCell, goalCell);
+
+				path = graphCells.BFS (graphCells.getNode (startCell), graphCells.getNode (goalCell));
 		} while(path==null);
 
-		/*
-		foreach (Node n in path) {
-			GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-			sphere.transform.position = new Vector3 (n.coords.x - m_mapWidth / 2, 0.5f, n.coords.y - m_mapHeight / 2);
-		}*/
-
-		graphTele = createTeleporters (graphCells,path,v);
-
-		//Create teleport areas
+		//Create teleporters and calculate/highlight areas of influence
+		graphTele = createTeleporters (graphCells, path, v);
 		foreach (Node n in graphCells.Nodes()) {
-			getTeleportAreas (n, v, graphCells, graphTele);
+				getTeleportAreas (n, v, graphCells, graphTele);
 		}
-		
+
 		//Create cell walls from edges
 		createWalls ();
-		
-		//Create floor
-		GameObject floor = Instantiate(prefab_floor) as GameObject; 
-		floor.transform.localScale = new Vector3 (10.0f,1.0f,5.0f);
-		floor.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
-		floor.transform.parent = transform;
 
+		//Create floor
+		/*
+		GameObject floor = Instantiate (prefab_floor) as GameObject; 
+		floor.transform.localScale = new Vector3 (10.0f, 1.0f, 5.0f);
+		floor.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		floor.transform.parent = transform;*/
+
+		createFloor ();
 		transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
 
 	}
@@ -126,12 +135,95 @@ public class LevelGenerator : MonoBehaviour
 
 	}
 
+	private void createFloor(){
+
+		for (int k=0; k<m_points.Count(); k++) {
+			Vector2 cell = m_points [k];		
+
+			List<Vector2> regions = v.Region (cell);
+			Vector3[] vertices = new Vector3[regions.Count + 1];
+			vertices [0] = new Vector3 (cell.x, 0.0f, cell.y);
+			for (int j=0; j<regions.Count(); j++) {
+					vertices [j + 1] = new Vector3 (regions [j].x, 0.0f, regions [j].y);
+			}
+
+			List<LineSegment> segments = v.VoronoiBoundaryForSite (cell);
+			List<Vector3> outerRing = new List<Vector3> ();
+			outerRing.Add (new Vector3 (segments [0].p0.Value.x, 0.0f, segments [0].p0.Value.y));
+			outerRing.Add (new Vector3 (segments [0].p1.Value.x, 0.0f, segments [0].p1.Value.y));
+			segments.RemoveAt (0);
+			while (segments.Count>0) {
+					Vector3 last = outerRing [outerRing.Count - 1];
+					for (int i=0; i<segments.Count(); i++) {
+							Vector3 p0 = new Vector3 (segments [i].p0.Value.x, 0.0f, segments [i].p0.Value.y);
+							Vector3 p1 = new Vector3 (segments [i].p1.Value.x, 0.0f, segments [i].p1.Value.y);
+							if (Vector3.Distance (p0, last) < 0.001) {
+									outerRing.Add (p1);
+									segments.RemoveAt (i);
+									break;
+							} else if (Vector3.Distance (p1, last) < 0.001) {
+									outerRing.Add (p0);
+									segments.RemoveAt (i);
+									break;
+							}
+					}
+			}
+
+			int[] triangles = new int[v.VoronoiBoundaryForSite (cell).Count () * 3];
+			int x = 0;
+			for (int i=0; i<outerRing.Count-1; i++) {
+
+					int ip0, ip1;
+					for (ip0=0; ip0<vertices.Count(); ip0++) {
+							if (Vector3.Distance (outerRing [i], vertices [ip0]) < 0.001) {
+									break;
+							}
+					}
+					for (ip1=0; ip1<vertices.Count(); ip1++) {
+							if (Vector3.Distance (outerRing [i + 1], vertices [ip1]) < 0.001) {
+									break;
+							}
+					}
+
+					triangles [x] = 0;
+					triangles [x + 1] = ip1;
+					triangles [x + 2] = ip0;
+					x += 3;
+			}
+
+			//Create triangle mesh representing the area of teleporter influence
+			Mesh mesh = new Mesh ();
+			mesh.Clear ();
+			mesh.vertices = vertices;
+			mesh.triangles = triangles;
+			mesh.RecalculateNormals ();
+
+			Vector2[] uvs = new Vector2[vertices.Length];
+			for (int i = 0; i < uvs.Count(); i++) {
+					uvs [i] = new Vector2 (vertices [i].x, vertices [i].z);
+			}
+			mesh.uv = uvs;
+
+			mesh.RecalculateBounds ();
+			mesh.Optimize ();
+
+			GameObject cellFloor = new GameObject ();
+			cellFloor.name = "CellFloor";
+			cellFloor.AddComponent (typeof(MeshFilter));
+			cellFloor.AddComponent (typeof(MeshRenderer));
+			cellFloor.GetComponent<MeshFilter> ().mesh = mesh;
+			cellFloor.renderer.material = material_floor;
+			cellFloor.transform.localPosition = new Vector3 (-m_mapWidth / 2, 0.0f, -m_mapHeight / 2);
+			cellFloor.transform.parent = gameObject_floor.transform;
+		}
+	}
+	
 	private GameGraph createTeleporters(GameGraph graphCells, List<Node> path, Delaunay.Voronoi v){
 
 		//Create new graph with nodes, but blank adjacency matrix
 		GameGraph tele = new GameGraph ();
 		foreach (Node n in graphCells.Nodes()) {
-			Node newNode = new Node(n.coords,n.type,n.id);
+			Node newNode = new Node(n.coords,n.type,n.id, n.minRadius);
 			tele.addNode(newNode);
 		}
 
@@ -169,18 +261,18 @@ public class LevelGenerator : MonoBehaviour
 			n2.addAdjacent(n1,commonEdge[0],commonEdge[1]);
 
 			//Instantiate teleporter at wall
-			createTeleporter((Vector2)commonEdge[0],(Vector2)commonEdge[1]);
+			createTeleporter((Vector2)commonEdge[0],(Vector2)commonEdge[1],teleporter);
 			m_teleporterCount--;
 		}*/
 
 		//Assing more teleporters on random points, if there are any teleporters left to distribute
 		for (int i=0; i<m_teleporterCount; i++) {
-			int rnd = Random.Range(0,graphCells.Nodes().Count()-1);
+			int rnd = UnityEngine.Random.Range(0,graphCells.Nodes().Count()-1);
 			Node rndNode = graphCells.Nodes()[rnd];
-			if(rndNode.getAdjacent().Count()>0){
-				rnd = Random.Range(0,rndNode.getAdjacent().Count()-1);
-				Node rndAdjacent = rndNode.getAdjacent()[rnd];
-				if(!tele.getNode(rndNode.id).getAdjacent().Contains(rndAdjacent)){
+			if(rndNode.getAdjacentCells().Count()>0){
+				rnd = UnityEngine.Random.Range(0,rndNode.getAdjacentCells().Count()-1);
+				Node rndAdjacent = rndNode.getAdjacentCells()[rnd];
+				if(!tele.getNode(rndNode.id).getAdjacentCells().Contains(rndAdjacent)){
 					List<Vector2> commonEdge = graphCells.getNode(rndNode.id).getEdgeToAdjacent(rndAdjacent);
 					rndNode.addAdjacent(rndAdjacent,commonEdge[0],commonEdge[1]);
 					rndAdjacent.addAdjacent(rndNode,commonEdge[0],commonEdge[1]);
@@ -255,7 +347,15 @@ public class LevelGenerator : MonoBehaviour
 		int id = 0;
 		GameGraph graphCells = new GameGraph ();
 		for (int i=0; i<m_points.Count; i++) {
-			graphCells.addNode(new Node(m_points[i], Node.CellType.ordinary, id++));
+			List<Vector2> region = v.Region(m_points[i]);
+			float minRadius = float.MaxValue;
+			foreach(Vector2 corner in region){
+				float dist = Vector2.Distance(corner,m_points[i]);
+				if(dist<minRadius){
+					minRadius = dist;
+				}
+			}
+			graphCells.addNode(new Node(m_points[i], Node.CellType.ordinary, id++, minRadius));
 		}
 		graphCells.getNode (startCell).type = Node.CellType.start;
 		graphCells.getNode (goalCell).type = Node.CellType.goal;
@@ -295,28 +395,57 @@ public class LevelGenerator : MonoBehaviour
 		List<List<Vector2>> ordinaryTris = new List<List<Vector2>> ();
 		Dictionary<List<Vector2>,Node> subdivision = new Dictionary<List<Vector2>,Node> ();
 		
-		if (graphTele.getNode (middle).getAdjacent ().Count == 0) {
+		if (graphTele.getNode (middle).getAdjacentCells().Count == 0) {
 			return subdivision;		
 		}
 		
 		int colorIndex = 0;
 		Dictionary<Node,Color> colorDict = new Dictionary<Node,Color> ();
-		List<Node> adjacentCells = graphCells.getNode (middle).getAdjacent ();
-		foreach(Node adj in adjacentCells){
+
+		//Distinguish edges next to teleporters and ordinary edges
+		List<List<Vector2>> teleporterEdges = new List<List<Vector2>> ();
+		List<Node> adjacentCells = graphCells.getNode(middle).getAdjacentCells();
+		foreach (Node adj in adjacentCells)
+		{
+			//Teleporter edge
 			List<Vector2> edge = graphTele.getNode(middle).getEdgeToAdjacent(adj);
-			if(edge !=null){
+			if (edge != null)
+			{
+				teleporterEdges.Add (edge);
 				edge.Add(middle);
 				subdivision.Add(edge, adj);
 				colorDict.Add(adj, colors[colorIndex]);
 				colorIndex++;
-			}else{
-				edge = graphCells.getNode(middle).getEdgeToAdjacent(adj);
-				edge.Add (middle);
-				ordinaryTris.Add (edge);
 			}
 		}
 
-		adjacentCells = graphTele.getNode (middle).getAdjacent ();
+		float epsilon = 0.001f;
+		List<List<Vector2>> ordinaryEdges = new List<List<Vector2>> ();
+		foreach (LineSegment ls in v.VoronoiBoundaryForSite (middle)) {
+			List<Vector2> edge = new List<Vector2>();
+			edge.Add ((Vector2)ls.p0);
+			edge.Add ((Vector2)ls.p1);
+			ordinaryEdges.Add (edge);
+		}
+
+		for(int i=0; i<ordinaryEdges.Count(); i++){
+			List<Vector2> edgeOrd = ordinaryEdges[i];
+			foreach(List<Vector2> edge in teleporterEdges){
+				if((Vector2.Distance(edge[0],edgeOrd[0])<epsilon && Vector2.Distance(edge[1],edgeOrd[1])<epsilon)||(Vector2.Distance(edge[0],edgeOrd[1])<epsilon && Vector2.Distance(edge[1],edgeOrd[0])<epsilon)){
+					ordinaryEdges.RemoveAt(i);
+					i--;
+					break;
+				}
+			}
+		}
+
+		foreach(List<Vector2> ordTri in ordinaryEdges){
+			ordTri.Add (middle);
+			ordinaryTris.Add (ordTri);
+		}
+
+		//Assign triangle to closest teleporter relative to the distance of the triangle midpoint to the middle of the teleporter edg
+		adjacentCells = graphTele.getNode (middle).getAdjacentCells ();
 		foreach(List<Vector2> tri in ordinaryTris){
 			float minDistance = float.MaxValue;
 			Node targetCell = null;
@@ -332,17 +461,12 @@ public class LevelGenerator : MonoBehaviour
 			}
 			subdivision.Add(tri, targetCell);
 		}
-		
+
 		foreach (KeyValuePair<List<Vector2>, Node> entry in subdivision) {
 			List<Vector2> tri = entry.Key;
 			Color c = colorDict[entry.Value];
 			createTriangle (new Vector3 (tri[0].x, 0.0f, tri[0].y), new Vector3 (tri[1].x, 0.0f, tri[1].y), new Vector3 (tri[2].x, 0.0f, tri[2].y), c);
 		}
-		
-		/*
-		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-		sphere.transform.position = new Vector3(n.coords.x-m_mapWidth/2,0.5f,n.coords.y-m_mapHeight/2);
-		*/
 		
 		return subdivision;
 	}
@@ -358,7 +482,7 @@ public class LevelGenerator : MonoBehaviour
 
     private GameObject getRandomEnemy()
     {
-        return m_enemies[(int)(Random.value % m_enemies.Count)];
+        return m_enemies[(int)(UnityEngine.Random.value % m_enemies.Count)];
     }
 
 	// Create a number of rectangles that can be traversed horizontally
@@ -373,17 +497,17 @@ public class LevelGenerator : MonoBehaviour
 		float x = compartment / 2;
 		
 		float y = m_mapHeight / 2;
-		float height = Random.Range (m_mapHeight/2,m_mapHeight);
+		float height = UnityEngine.Random.Range (m_mapHeight/2,m_mapHeight);
 		rs.Add (new Rectangle (new Vector2 (x,y), width, height));
 		
 		for (int i=1; i<nRectangles; i++) {
 			x = compartment*i + compartment / 2;
-			height = Random.Range (minHeight,m_mapHeight);
+			height = UnityEngine.Random.Range (minHeight,m_mapHeight);
 			
 			float lowerBoundary = minHeight = rs[i-1].middle.y-(rs[i-1].height/2-minOverlap)-height/2;
 			float upperBoundary = rs[i-1].middle.y+(rs[i-1].height/2-minOverlap)+height/2;
 			
-			y = Random.Range (Mathf.Max(height/2,lowerBoundary), Mathf.Min(m_mapHeight-height/2,upperBoundary)); 
+			y = UnityEngine.Random.Range (Mathf.Max(height/2,lowerBoundary), Mathf.Min(m_mapHeight-height/2,upperBoundary)); 
 			rs.Add (new Rectangle (new Vector2 (x,y), width, height));
 		}
 		
@@ -395,7 +519,7 @@ public class LevelGenerator : MonoBehaviour
 		//Create vector of random seed points in euclidean space
 		List<Vector2> points = new List<Vector2> (); 
 		while (points.Count < m_pointCount) {
-			Vector2 point = new Vector2 (Random.Range (0, m_mapWidth), Random.Range (0, m_mapHeight));
+			Vector2 point = new Vector2 (UnityEngine.Random.Range (0, m_mapWidth), UnityEngine.Random.Range (0, m_mapHeight));
 			foreach (Rectangle r in rects) {
 				if (r.isWithin (point)) {
 					points.Add (point);
@@ -428,7 +552,7 @@ public class LevelGenerator : MonoBehaviour
 			float angle = -Mathf.Atan2(p0.y - p1.y, p0.x - p1.x) * (180 / Mathf.PI) + 90;
 			GameObject wall = Instantiate(prefab_wall,new Vector3(midpoint.x-m_mapWidth/2,0.5f*m_wallHeight,midpoint.y-m_mapHeight/2),Quaternion.Euler(new Vector3(0.0f,angle,0.0f))) as GameObject;
 			wall.transform.localScale = new Vector3(0.1f,1.0f*m_wallHeight,length);
-			wall.transform.parent = transform;
+			wall.transform.parent = gameObject_walls.transform;
 
 		}
 	}
@@ -441,7 +565,7 @@ public class LevelGenerator : MonoBehaviour
 		float angle = -Mathf.Atan2(p0.y - p1.y, p0.x - p1.x) * (180 / Mathf.PI) + 90;
 		GameObject teleporter = Instantiate(prefab_teleporter,new Vector3(midpoint.x-m_mapWidth/2,0.5f*m_wallHeight,midpoint.y-m_mapHeight/2),Quaternion.Euler(new Vector3(0.0f,angle,0.0f))) as GameObject; 
 		teleporter.transform.localScale = new Vector3(0.5f,0.8f*m_wallHeight,length);
-		teleporter.transform.parent = transform;
+		teleporter.transform.parent = gameObject_teleporters.transform;
 		
 	}
 
@@ -477,16 +601,17 @@ public class LevelGenerator : MonoBehaviour
 		triangle.GetComponent<MeshFilter> ().mesh = mesh;
 		
 		//Material myNewMaterial = new Material(Shader.Find("Transparent/Diffuse"));
-		Material myNewMaterial = new Material(Shader.Find("Transparent/Diffuse"));
+		Material myNewMaterial = new Material (Shader.Find("Transparent/Diffuse"));
 
 		myNewMaterial.SetColor ("_Color", c);
 		triangle.renderer.material = myNewMaterial;
 		
 		triangle.transform.localPosition=new Vector3(-m_mapWidth/2, 0.1f, -m_mapHeight/2);
-		triangle.transform.parent = transform;
+		triangle.transform.parent = gameObject_triangle.transform;
 
 	}
-	
+
+	/*
 	// Drawing method for debugging
 	private void OnDrawGizmos ()
 	{
@@ -509,17 +634,7 @@ public class LevelGenerator : MonoBehaviour
 				Gizmos.DrawLine ((Vector3)left, (Vector3)right);
 			}
 		}
-		
-		/*
-		Gizmos.color = Color.magenta;
-		if (m_delaunayTriangulation != null) {
-			for (int i = 0; i< m_delaunayTriangulation.Count; i++) {
-				Vector2 left = (Vector2)m_delaunayTriangulation [i].p0;
-				Vector2 right = (Vector2)m_delaunayTriangulation [i].p1;
-				Gizmos.DrawLine ((Vector3)left, (Vector3)right);
-			}
-		}
-		*/
+
 		//Draw boundaries
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawLine (new Vector2 (0, 0), new Vector2 (0, m_mapHeight));
@@ -527,4 +642,5 @@ public class LevelGenerator : MonoBehaviour
 		Gizmos.DrawLine (new Vector2 (m_mapWidth, 0), new Vector2 (m_mapWidth, m_mapHeight));
 		Gizmos.DrawLine (new Vector2 (0, m_mapHeight), new Vector2 (m_mapWidth, m_mapHeight));
 	}
+	*/
 }
