250f394b93ccb3582efacc4e01fd24d9a893447b
Nick Sephton <njs523@york.ac.uk>
2014-11-26 15:01:32 +0000

    Removed all the NavAgent stuff, new "AI" for enemies Added to layers to the TagManager.asset

--- Assets/Scripts/Enemies/EnemyAI_BasicCollider.cs
+++ Assets/Scripts/Enemies/EnemyAI_BasicCollider.cs
@@ -10,6 +10,7 @@ public class EnemyAI_BasicCollider : MonoBehaviour
     private CharacterController PlayerController;
     private CharacterController Controller;
     private bool HasSeenPlayer;
+    private Vector3 lastKnownPlayerLoc;
 
     // Use this for initialization
     void Start()
@@ -44,30 +45,57 @@ public class EnemyAI_BasicCollider : MonoBehaviour
             Vector3 playerDir = playerColliderLoc - this.transform.position;
 
             Ray newRay = new Ray(this.transform.position, playerDir);
-            Debug.DrawRay(this.transform.position, playerDir);
+            //Debug.DrawRay(this.transform.position, playerDir);
             RaycastHit info;
 
-            //Try to lock the player 
+            //Have we seen the player?
             if (!HasSeenPlayer)
             {
-                //by casting a ray from this location
-                if (Physics.Raycast(newRay, out info, 100f, 1 << 8))
+                //If not, try to find them by casting a ray from this location
+                if (Physics.Raycast(newRay, out info, 100f))
                 {
                     if (info.collider == PlayerController)
                     {
                         //We can see the player
                         HasSeenPlayer = true;
+                        lastKnownPlayerLoc = info.collider.transform.position;
                     }
                 }
             }
 
-            //We've seen the player and they're too far away, so use the navAgent to chase them
-            if (HasSeenPlayer && Vector3.Distance(playerColliderLoc, this.transform.position) > PursuitDistance)
-            {
-                //Do AI Stuff here!
-                this.transform.LookAt(playerColliderLoc);
 
-                Controller.Move(newRay.direction * Time.deltaTime * Speed);
+            if(HasSeenPlayer)
+            { 
+                //Can we still see them?
+                if (Physics.Raycast(newRay, out info, 100f))
+                {
+                    //Update their location
+                    lastKnownPlayerLoc = info.collider.transform.position;
+
+                    //Turn to face them
+                    this.transform.LookAt(playerColliderLoc);
+
+                    //Check how far away 
+                    if (Vector3.Distance(playerColliderLoc, this.transform.position) > PursuitDistance)
+                    {
+                        //Can see them, but they're far away, close in.
+                        Controller.Move(newRay.direction * Time.deltaTime * Speed);
+                    }
+                    else
+                    {
+                        //We're close, just stand here (attack scripts will fire)
+                    }
+                }
+                else if(Physics.Raycast(newRay, out info, 100f, 1 << 9))
+                {
+                    //Cheat by looking through local obstacles to see if they're hiding behind something
+
+                }
+                else
+                {
+                    //They've left the cells, so stop or try to teleport after them
+
+                }
             }
         }
     }
