563935e16f2d42c657894bd6ffe2256fcc8e866c
bedder <bedder@gmail.com>
2014-11-27 17:16:14 +0000

    Added code to move between levels. This requires that a GameController exists (which defines which level to switch to next)

--- Assets/Scripts/Map/Objects/Goal.cs
+++ Assets/Scripts/Map/Objects/Goal.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class Goal : MonoBehaviour {
     public float range = 1f;
+
     private Transform playerTransform;
+    private GameController gameController;
 
 	void Start () {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        gameController = GameObject.FindObjectOfType<GameController>();
 	}
 	
 	void Update () {
         Vector3 delta = playerTransform.position - transform.position;
         if (playerTransform != null) {
             if (delta.magnitude <= range) {
-                Debug.Log("In range of goal!"); // TODO
+                Application.LoadLevel(gameController.nextLevel);
             }
         }
 	}
