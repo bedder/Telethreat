687300273cb17a4312cb91148e3f9fe6d7976c59
bedder <bedder@gmail.com>
2015-07-05 19:55:53 +0100

    Updated main menu to use modern UI features

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,133 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-    public GUIStyle buttonStyle;
-    public GUIStyle classNameStyle;
-    public GUIStyle classDescriptionStyle;
-    public GUIStyle musicCreditsStyle;
-
-    public GameObject lightInfantryPrefab;
-    public GameObject heavyInfantryPrefab;
-    public GameObject engineerPrefab;
-    public GameObject medicPrefab;
-    private GameController gameController;
-
-    private int padX = 20;
-    private int padY = 5;
-    private int buttonWidth = 100;
-    private int classNameWidth = 300;
-    private int classDescriptionWidth = 350;
-    private int buttonHeight = 20;
-    
-    public Texture2D logo;
-    private Rect logoLocation;
-
-    private Rect buttonsLocation;
-    private Rect playButton;
-    private Rect quitButton;
-
-    private bool showClasses = false;
-    private Rect classesLocation;
-    private Rect lightButton;
-    private Rect heavyButton;
-    private Rect enginButton;
-    private Rect medicButton;
+    public GameObject[] PlayerControllers;
+    public GameObject ExitButton;
+    public GameObject ClassSelect;
+    public Text DescriptionText;
 
     private int selectedClass;
-
-    private Rect infopaneLocation;
-    private Rect className;
-    private Rect classDescription;
-
-    private Rect startButton;
-
-    private Rect musicCredits;
-
-    private string[] classNames;
-    private string[] classDescriptions;
+    private GameController gameController;
 
 	void Start () {
         gameController = GameObject.FindObjectOfType<GameController>();
 
-        int offset = padY;
-        if (logo != null) {
-            logoLocation = new Rect((Screen.width - logo.width) / 2, padY, logo.width, logo.height);
-            offset += logo.height + padY;
-        }
-
-        int requiredSpace = 2 * padX + buttonWidth + classNameWidth + classDescriptionWidth;
-
-        buttonsLocation = new Rect((Screen.width - requiredSpace) / 2, offset, buttonWidth, 2 * buttonHeight + padY);
-        playButton = new Rect(0, 0, buttonWidth, buttonHeight);
-        quitButton = new Rect(0, buttonHeight + padY, buttonWidth, buttonHeight);
-
-        classesLocation = new Rect(buttonsLocation.x + buttonsLocation.width + padX, offset, classNameWidth, 4 * buttonHeight + 3 * padY);
-        lightButton = new Rect(0, 0, classNameWidth, buttonHeight);
-        heavyButton = new Rect(0, 1 * (buttonHeight + padY), classNameWidth, buttonHeight);
-        enginButton = new Rect(0, 2 * (buttonHeight + padY), classNameWidth, buttonHeight);
-        medicButton = new Rect(0, 3 * (buttonHeight + padY), classNameWidth, buttonHeight);
-
-        infopaneLocation = new Rect(classesLocation.x + classesLocation.width + padX, offset, classDescriptionWidth, 75 + padX + buttonHeight);
-        className = new Rect(0, 0, infopaneLocation.width, 20);
-        classDescription = new Rect(0, 25, infopaneLocation.width, 100);
-        startButton = new Rect(infopaneLocation.width - buttonWidth, 100, buttonWidth, buttonHeight);
-
-        classNames = new string[4] { "Light Infantry", "Heavy Infantry", "Engineer", "Medic" };
-        classDescriptions = new string[4] { "Want to run around? The light infantry might be for you. He's not quite as heavily armoured as the other classes but he does have the ability to sprint. Handy with all these enemies around.",
-                                            "Everyone likes explosions, it's a fact of life, but the heavy infantry likes them a little bit more than the average person. Throw grenades without the enrgy cost by pressing the class key (Default: ENTER or F).", "In theory the engineer is able to slow down the teleportation timer, meaning that you can control the flow of the game a bit better. But engineering is hard, and this feature is still in the process of being implemented.", "Medics are great for multiplayer as they allow you to heal your allies. Multiplayer is a stretch-goal on our kickstarter. So in reality the medic is just a worse infantryman." };
-
-        musicCredits = new Rect(0, Screen.height - 20, Screen.width, 20);
+        if (Application.isWebPlayer)
+            ExitButton.SetActive(false);
 	}
-	
-    void OnGUI() {
-        if (logo != null) {
-            GUI.DrawTexture(logoLocation, logo);
-        }
 
-        GUI.BeginGroup(buttonsLocation);
-            if (GUI.Button(playButton, "Play", buttonStyle))
-                showClasses = true;
-            if (GUI.Button(quitButton, "Exit", buttonStyle))
-                Application.Quit();
-        GUI.EndGroup();
+    public void ShowClasses() {
+        ClassSelect.SetActive(true);
+    }
 
-        if (showClasses) {
-            GUI.BeginGroup(classesLocation);
-                if (GUI.Button(lightButton, classNames[0], buttonStyle))
-                    selectedClass = 0;
-                if (GUI.Button(heavyButton, classNames[1], buttonStyle))
-                    selectedClass = 1;
-                if (GUI.Button(enginButton, classNames[2], buttonStyle))
-                    selectedClass = 2;
-                if (GUI.Button(medicButton, classNames[3], buttonStyle))
-                    selectedClass = 3;
-            GUI.EndGroup();
+    public void SelectClass(int i) {
+        selectedClass = i;
+        updateDescriptionText();
+    }
 
-            GUI.BeginGroup(infopaneLocation);
-                GUI.Label(className, classNames[selectedClass], classNameStyle);
-                GUI.Label(classDescription, classDescriptions[selectedClass], classDescriptionStyle);
-                if (GUI.Button(startButton, "Start!", buttonStyle)) {
-                    switch (selectedClass) {
-                        case 0:
-                            gameController.playerController = lightInfantryPrefab;
-                            break;
-                        case 1:
-                            gameController.playerController = heavyInfantryPrefab;
-                            break;
-                        case 2:
-                            gameController.playerController = engineerPrefab;
-                            break;
-                        case 3:
-                            gameController.playerController = medicPrefab;
-                            break;
-                    }
-                    Application.LoadLevel(gameController.nextLevel);
-                }
-            GUI.EndGroup();
+    public void StartTheGame() {
+        gameController.playerController = PlayerControllers[selectedClass];
+        Application.LoadLevel(1);
+    }
+
+    public void LoadAbout() {
+        Application.OpenURL("http://bedder.co.uk/games/telethreat/details/");
+    }
+
+    private void updateDescriptionText() {
+        switch (selectedClass) {
+            case 0:
+                DescriptionText.text =
+                    "Want to run around? The <color=#FF76FB>light "
+                    + "infantry</color> might be for you. He's not quite as "
+                    + "heavily armoured as the other classes but he does have "
+                    + "the ability to sprint. Handy with all these enemies "
+                    + "around.";
+                break;
+            case 1:
+                DescriptionText.text =
+                    "Everyone likes explosions, it's a fact of life, but the "
+                    + "<color=#FF76FB>heavy infantry</color> likes them a little "
+                    + "bit more than the average person. Throw grenades without "
+                    + "the energy cost by pressing the class key (Default: "
+                    + "ENTER or F).";
+                break;
+            case 2:
+                DescriptionText.text =
+                    "In theory the <color=#FF76FB>engineer</color> is able to "
+                    + "slow down the teleportation timer, meaning that you can "
+                    + "control the flow of the game a bit better. But "
+                    + "engineering is hard, and this feature is still in the "
+                    + "process of being implemented.";
+                break;
+            case 3:
+                DescriptionText.text =
+                    "<color=#FF76FB>Medics</color> are great for multiplayer as "
+                    + "they allow you to heal your allies. Multiplayer is a "
+                    + "stretch-goal on our kickstarter. So in reality the medic "
+                    + "is just a worse infantryman.";
+                break;
         }
-        if (GUI.Button(musicCredits, "Music by Rakohus (www.rakoh.us)", musicCreditsStyle)) {
-            Application.OpenURL("http://www.rakoh.us");
-        }
-	}
+    }
+
+
 }
